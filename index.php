73866f8affb33a20d1fbb69be1b926434438ee6f
Daniil Selivirov <selil.steele@gmail.com>
2022-09-12 10:21:18 +0700

    инит проекта

--- index.php
+++ index.php
@@ -3,22 +3,3 @@
 
 
 ?>
-
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--
-    <link href="/bootstrap-5.2.1-dist/css/bootstrap.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.6.1.js" integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI=" crossorigin="anonymous"></script>
-    -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <script src="jquery.js"></script>
-    <title>Мемес</title>
-</head>
-<body>
-    <div></div>
-</body>
-</html>
